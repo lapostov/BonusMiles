922837cc3dc27a6c8864ffaee64114b8d66f762f
Александр Масляков <lapostov@yandex.ru>
2022-07-20 14:51:28 +0400

    Create first

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Здравствуйте");
+        int amount = 3000; // Сумма покупки
+        int bonus = amount / 20; // Количество полученных бонусов
+        System.out.println("Вы получите " + bonus + " бонусных миль");
+    }
+}
